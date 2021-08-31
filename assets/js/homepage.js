adaee9e39ec280f86360e85473306bf889fe6d54
edgardopina <edgardopina@yahoo.com>
2021-08-31 02:39:39 -0600

    Added display of repositories with icons and count of closed and open issues.

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,28 +1,65 @@
-var getUserRepos = function (user) {
-	// console.log("function was called");
-	// var response = fetch("https://api.github.com/users/octocat/repos");
-	// console.log(response);
+var repoContainerEl = document.querySelector("#repos-container");
+var repoSearchTerm = document.querySelector("#repo-search-term");
 
-	// the .them method is called when the promise is fullfilled
-	// fetch("https://api.github.com/users/octocat/repos").then(function (response) {
-	// 	console.log("inside ", response);
-	// });
+var displayRepos = function (repos, searchTerm) {
+	// clear old content
+	repoContainerEl.textContent = "";
+	repoSearchTerm.textContent = searchTerm;
+	// loop over repos
+	for (var i = 0; i < repos.length; ++i) {
+		// format repo name
+		var repoName = repos[i].owner.login + "/" + repos[i].name;
+		// create container for each repo
+		var repoEl = document.createElement("div");
+		repoEl.classList = "list-item flex-row justify-space-between align-center";
+		// create span element to hold repository name
+		var titleEl = document.createElement("span");
+		titleEl.textContent = repoName;
+		// append to container
+		repoEl.appendChild(titleEl);
 
-	// fetch("https://api.github.com/users/octocat/repos").then(function (response) {
-	// 	response.json().then(function (data) {
-	// 		console.log(data);
-	// 	});
-   // });
-   
-   // format the github api url
-   var apiUrl = "https://api.github.com/users/" + user + "/repos";
-   // make a request to the url
-   fetch(apiUrl).then(function (response) {
-		response.json().then(function (data) {
-			console.log(data);
-		});
-	});
-   
+		// create a status element
+		var statusEl = document.createElement("span");
+		statusEl.classList = "flex-row align-center";
+		//check if current repo has issues or not
+		if (repos[i].open_issues_count > 0) {
+			statusEl.innerHTML =
+				"<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + " issue(s)";
+		} else {
+			statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
+      }
+      // append to container
+      repoEl.appendChild(statusEl);
+
+		// append container to the DOM
+		repoContainerEl.appendChild(repoEl);
+	}
 };
 
-getUserRepos("edgardopina");
+var formSubmitHandler = function (event) {
+	event.preventDefault();
+	// get value from input element
+	var username = nameInputEl.value.trim();
+	if (username) {
+		getUserRepos(username);
+		nameInputEl.value = "";
+	} else {
+		window.alert("Please enter a GitHub username");
+	}
+};
+
+var getUserRepos = function (user) {
+	// format the github api url
+	var apiUrl = "https://api.github.com/users/" + user + "/repos";
+	// make a request to the url
+	fetch(apiUrl).then(function (response) {
+		response.json().then(function (data) {
+			displayRepos(data, user);
+		});
+	});
+};
+
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+
+userFormEl.addEventListener("submit", formSubmitHandler);
