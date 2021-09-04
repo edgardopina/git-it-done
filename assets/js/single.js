cb018c68185d6c68bbe11c7aa836797ed61916df
edgardopina <edgardopina@yahoo.com>
2021-09-04 17:19:35 -0600

    Added limit warning

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,5 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
 
 var getRepoIssues = function (repo) {
    var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -8,7 +9,14 @@ var getRepoIssues = function (repo) {
          if (response.ok) {
             response.json()
                .then(function (data) {
+                  console.log(response);
                   displayIssues(data);
+
+                  // check if api has paginated issues
+                  if (response.headers.get("Link")) {
+                     displayWarning(repo);
+                  }
+
                });
          } else {
             window.alert("There was a problem with your request");
@@ -27,7 +35,7 @@ var displayIssues = function (issues) {
       issueEl.classList = "list-item flex-row justify-space-between align-center";
       issueEl.setAttribute("href", issues[i].html_url);
       issueEl.setAttribute("target", "_blank");
-      
+
       // create span to hold issue title
       var titleEl = document.createElement("span");
       titleEl.textContent = issues[i].title;
@@ -47,8 +55,20 @@ var displayIssues = function (issues) {
 
       // append to container
       issueEl.appendChild(typeEl);
-      
+
       issueContainerEl.appendChild(issueEl);
    }
+}
+
+var displayWarning = function (repo) {
+   // add text to warning container
+   limitWarningEl.textContent = "To see more than 30 issues, visit: ";
+   var linkEl = document.createElement("a");
+   linkEl.textContent = "See More Issues on GitHub.com";
+   linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+   linkEl.setAttribute("target", "_blank");
+
+   // append to warning container
+   limitWarningEl.appendChild(linkEl);
 }
 getRepoIssues("facebook/react");
