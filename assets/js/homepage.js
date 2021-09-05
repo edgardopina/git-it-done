d1aca8ce06728bed2a0242f5061efcd759148d7c
edgardopina <edgardopina@yahoo.com>
2021-09-04 21:26:53 -0600

    Complete language buttons implementation.

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,8 +2,23 @@ var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
 var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
+var languageButtonsEl = document.querySelector("#language-buttons");
 
 
+var getFeaturedRepos = function (language) {
+   var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+
+   fetch(apiUrl)
+      .then(function (response) {
+         if (response.ok) {
+            response.json().then(function (data) {
+               displayRepos(data.items, language);
+            });
+         } else {
+            alert('Error: GitHub User Not Found');
+         }
+      });
+};
 
 var displayRepos = function (repos, searchTerm) {
    // check if api returned any repos
@@ -72,6 +87,16 @@ var getUserRepos = function (user) {
       });
 };
 
+var buttonClickHandler = function (event) {
+   var language = event.target.getAttribute("data-language");
+   console.log(language);
+   if (language) {
+      getFeaturedRepos(language);
+      // clear old content
+      repoContainerEl.textContent = "";
+   }
+}
+
 var formSubmitHandler = function (event) {
    event.preventDefault();
    // get value from input element
@@ -86,4 +111,4 @@ var formSubmitHandler = function (event) {
 
 
 userFormEl.addEventListener("submit", formSubmitHandler);
-
+languageButtonsEl.addEventListener("click", buttonClickHandler);
